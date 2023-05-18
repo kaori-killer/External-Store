f30911fff003447ecca5438940a221ed02bf35b8
kaori-killer <sksbthwjd@daum.net>
2023-05-19 04:24:16 +0900

    test 코드 성공 / 증가 버튼 2번 눌렀을 때

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,17 +2,22 @@ import {render, screen, fireEvent} from '@testing-library/react';
 
 import App from './App';
 
+const context = describe;
+
 test('App', () => {
 	render(<App />);
 });
 
 describe('App', () => {
-	test('counter', () => {
-		render(<App />);
+	context('', () => {
+		test('counter', () => {
+			render(<App />);
 
-		fireEvent.click(screen.getByText('증가'));
+			fireEvent.click(screen.getByText('증가'));
+			fireEvent.click(screen.getByText('증가'));
 
-		const elements = screen.getAllByText('Counter: 1');
-		expect(elements).toHaveLength(2);
+			const elements = screen.getAllByText('Counter: 2');
+			expect(elements).toHaveLength(2);
+		});
 	});
 });
