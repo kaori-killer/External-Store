23353daf388753ea63d7af90aa002e530c3260ed
kaori-killer <sksbthwjd@daum.net>
2023-05-19 04:05:00 +0900

    증가/감소 구현

--- src/components/CounterControl.tsx
+++ src/components/CounterControl.tsx
@@ -7,16 +7,24 @@ import Store from '../stores/Store';
 export default function CounterControl() {
 	const store = container.resolve(Store);
 
-	const handleClick = () => {
+	const handleClickIncrease = () => {
 		store.count += 1;
+		store.update();
+	};
+
+	const handleClickDecrease = () => {
+		store.count -= 1;
 		store.update();
 	};
 
 	return (
 		<div>
 			<p>{store.count}</p>
-			<button type='button' onClick={handleClick}>
+			<button type='button' onClick={handleClickIncrease}>
                 증가
+			</button>
+			<button type='button' onClick={handleClickDecrease}>
+                감소
 			</button>
 		</div>
 	);
